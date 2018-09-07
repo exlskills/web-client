7816faee424a97782a379b12e030cfb5997a2b40
Sasha Varlamov <sasha.varlamov@exlhub.com>
2018-09-07 11:03:58 -0700

    Update theme colors

--- app/common/styles/themeConstants.ts
+++ app/common/styles/themeConstants.ts
@@ -1,17 +1,35 @@
 import { ITheme } from 'typings/client'
 
-export const darkTheme: ITheme = {
-  background: '#1B232F',
-  text: '#E6E7E2',
-  primary: '#44638A',
-  secondary: '#3671B6',
-  secondaryInverse: '#8E8BA3'
-}
+// Previous colors
+// export const darkTheme: ITheme = {
+//   background: '#1B232F',
+//   text: '#E6E7E2',
+//   primary: '#44638A',
+//   secondary: '#3671B6',
+//   secondaryInverse: '#8E8BA3'
+// }
+//
+// export const lightTheme: ITheme = {
+//   background: '#FFFFFF',
+//   text: '#1B232F',
+//   primary: '#44638A',
+//   secondary: '#8E8BA3',
+//   secondaryInverse: '#3671B6'
+// }
 
+// Latest colors
 export const lightTheme: ITheme = {
   background: '#FFFFFF',
-  text: '#1B232F',
-  primary: '#44638A',
-  secondary: '#8E8BA3',
-  secondaryInverse: '#3671B6'
+  text: '#0A1E3C',
+  primary: '#0C61E1',
+  secondary: '#D2D2D7',
+  secondaryInverse: '#0A3F8E'
+}
+
+export const darkTheme: ITheme = {
+  background: '#1C222E',
+  text: '#FFFFFF',
+  primary: '#182F52',
+  secondary: '#2B303C',
+  secondaryInverse: '#616570'
 }
